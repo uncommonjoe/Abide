44e87efcd1dffc6ddd0275dec3b862b1a9d22072
Joe McLean <uncommonjoe@gmail.com>
2022-04-27 19:07:11 -0600

    Added logic to get track data

--- src/api/ReadingPlanAPI.js
+++ src/api/ReadingPlanAPI.js
@@ -6,33 +6,31 @@
 // });
 
 // export function readingPlanAPI() {
-//     const [isLoading, setLoading] = useState(true);
+// 	const [isLoading, setLoading] = useState(true);
 // 	const [data, setData] = useState([]);
 
-// 	return (
+// 	const getPlan = async () => {
+// 		try {
+// 			const response = await fetch(
+// 				'https://cornerstonebillings.org/api/abide-small.json'
+// 			);
+// 			const json = await response.json();
+// 			setData(json.plans);
+// 			return data;
+// 		} catch (error) {
+// 			console.error(error);
+// 		} finally {
+// 			setLoading(false);
+// 		}
+// 	};
 
-//         const getPlan = async () => {
-//             	try {
-//             		const response = await fetch(
-//             			'https://cornerstonebillings.org/api/abide-small.json'
-//             		);
-//             		const json = await response.json();
-//             		setData(json.plans);
-//             	} catch (error) {
-//             		console.error(error);
-//             	} finally {
-//             		setLoading(false);
-//             	}
-//             };
-
-// 		// fetch(`https://cornerstonebillings.org/api/abide-small.json`)
-// 		// 	//   , {headers: {"Authorization": "Bearer " + this.user_token}
-// 		// 	.then((response) => response.json())
-// 		// 	.then((responseJson) => {
-// 		// 		return responseJson.plans;
-// 		// 	})
-// 		// 	.catch((error) => {
-// 		// 		console.error(error);
-// 		// 	})
-// 	);
-// }
+// fetch(`https://cornerstonebillings.org/api/abide-small.json`)
+// 	//   , {headers: {"Authorization": "Bearer " + this.user_token}
+// 	.then((response) => response.json())
+// 	.then((responseJson) => {
+// 		return responseJson.plans;
+// 	})
+// 	.catch((error) => {
+// 		console.error(error);
+// 	})
+//}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import Moment from 'moment';
 import { StatusBar } from 'expo-status-bar';
 import styles from '../assets/styles/container.style';
 import { TitleText, Text } from '../assets/styles/Text';
+import { filter, pick } from 'lodash';
 import { useNavigation } from '@react-navigation/native';
 
 export default function HomePage() {
@@ -17,16 +18,56 @@ export default function HomePage() {
 	const [data, setData] = useState(null);
 	const navigation = useNavigation();
 
+	const userObj = {
+		myTrack: 'track 2',
+	};
+
 	let today = new Date();
 	let date = Moment(today).format('dd, MMM d, YYYY');
+	let todaysObject, myTrack;
 
 	const getPlan = async () => {
 		try {
 			const response = await fetch(
-				'https://cornerstonebillings.org/api/abide-small.json'
+				'https://cornerstonebillings.org/api/abide-small.json?v=1'
 			);
 			const json = await response.json();
-			setData(json.plans);
+
+			// Filter plans and get the object that matches todays date
+			filter(json.plans, function (p) {
+				if (p.date == 'Tue, Jul 5, 2022') {
+					// Filter track by users selected track
+					filter(p.tracks, function (t) {
+						if ((t = userObj.myTrack)) {
+							myTrack = t;
+
+							// Create collection of tracks based on users selected track
+							if (myTrack == 'track 1') {
+								todaysObject = pick(p.tracks[0], ['track 1']);
+							} else if (myTrack == 'track 2') {
+								todaysObject = pick(p.tracks[0], [
+									'track 1',
+									'track 2',
+								]);
+							} else if (myTrack == 'track 3') {
+								todaysObject = pick(p.tracks[0], [
+									'track 1',
+									'track 2',
+									'track 3',
+								]);
+							}
+
+							setData(todaysObject);
+						}
+					});
+				}
+			});
+			console.log(todaysObject);
+
+			// todaysObject = filter(json.plans, function (o) {
+			// 	return o.date == 'Tue, Jul 5, 2022'; // should be today's date
+			// });
+			// console.log(todaysObject[0]);
 		} catch (error) {
 			console.error(error);
 		} finally {
@@ -52,7 +93,7 @@ export default function HomePage() {
 						data={data}
 						renderItem={({ item, index }) => (
 							<View>
-								<Text>{item.date}</Text>
+								<Text>{item[0]}</Text>
 							</View>
 						)}
 					/>
