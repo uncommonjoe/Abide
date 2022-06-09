3e8fdf2483f5e658b7b66ef2d9d63be962acfc39
Yasir Wahid <yasirwahid2@gmail.com>
2022-06-09 15:50:23 -0400

    modified header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import { Text, TitleText } from '../assets/styles/Text';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
-import CircleCheck from './CircleCheck';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Text, TitleText } from "../assets/styles/Text";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import CircleCheck from "./CircleCheck";
 
 //https://docs.expo.dev/guides/using-custom-fonts/
 // import {
@@ -12,66 +12,64 @@ import CircleCheck from './CircleCheck';
 // 	Montserrat_700Bold,
 // } from '@expo-google-fonts/montserrat';
 
-const Header = ({ title }) => {
-	return (
-		<View style={styles.header}>
-			<Text style={styles.title}>{title.passage}</Text>
+const Header = ({ title, onChange = () => {} }) => {
+  //   console.warn("data", title);
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>{title.passage}</Text>
 
-			<View style={styles.buttonsWrap}>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => alert('Listen!')}
-					color='blue'
-				>
-					<FontAwesomeIcon
-						icon={faVolumeHigh}
-						color={'#454C57'}
-						size={36}
-					/>
-				</TouchableOpacity>
+      <View style={styles.buttonsWrap}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert("Listen!")}
+          color="blue"
+        >
+          <FontAwesomeIcon icon={faVolumeHigh} color={"#454C57"} size={36} />
+        </TouchableOpacity>
 
-				<CircleCheck
-					state={false}
-					color={'#454C57'}
-					onPress={() => alert('Complete')}
-				/>
-			</View>
-		</View>
-	);
+        <CircleCheck
+          isComplete={title.isComplete}
+          readingObj={title}
+          color={"#454C57"}
+          onChange={onChange}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	header: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		backgroundColor: '#F3F2F8',
-		width: '100%',
-		padding: 20,
-		paddingTop: 60,
-	},
-	title: {
-		color: '#454C57',
-		//fontFamily: Montserrat_700Bold,
-		fontSize: 24,
-		textTransform: 'uppercase',
-	},
-	buttonsWrap: {
-		flexDirection: 'row',
-		alignItems: 'center',
-	},
-	button: {
-		backgroundColor: 'transparent',
-		marginHorizontal: 10,
-		padding: 10,
-		flexDirection: 'row',
-		alignItems: 'center',
-	},
-	buttonText: {
-		marginLeft: 10,
-		//fontFamily: Montserrat_700Bold,
-		textTransform: 'uppercase',
-	},
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#F3F2F8",
+    width: "100%",
+    padding: 20,
+    paddingTop: 60,
+  },
+  title: {
+    color: "#454C57",
+    //fontFamily: Montserrat_700Bold,
+    fontSize: 24,
+    textTransform: "uppercase",
+  },
+  buttonsWrap: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "transparent",
+    marginHorizontal: 10,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonText: {
+    marginLeft: 10,
+    //fontFamily: Montserrat_700Bold,
+    textTransform: "uppercase",
+  },
 });
 
 export default Header;
