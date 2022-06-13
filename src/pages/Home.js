1e4ca54c52eaaca6fb08da4d32d3a497fbc754a0
Yasir Wahid <yasirwahid2@gmail.com>
2022-06-13 15:37:37 -0400

    added bottom sheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import Moment from "moment";
 import TodaysReading from "../components/TodaysReading";
 import Calendar from "../components/Calendar";
 import userReadings from "../utils/hooks/userReadings";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
 
 const HomePage = ({ setHeaderTitle }) => {
+  const setRef = useRef(null);
+  const [modal, setModal] = useState(false);
   const { usrReadings } = userReadings();
   global.userReadings = usrReadings;
   global.planTitle = "2022-2023";
@@ -24,7 +36,7 @@ const HomePage = ({ setHeaderTitle }) => {
 
   return (
     <View style={styles.container}>
-      <SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
         <StatusBar style="dark" />
 
         <View style={styles.section}>
@@ -35,6 +47,23 @@ const HomePage = ({ setHeaderTitle }) => {
           selectedDay={selectedDay}
           setHeaderTitle={setHeaderTitle}
         />
+        <TouchableOpacity
+          onPress={() => {
+            setModal(true);
+            setRef.current.present();
+          }}
+        >
+          <Text>Open picker</Text>
+        </TouchableOpacity>
+        <BottomSheetModalProvider>
+          <BottomSheetModal
+            ref={setRef}
+            snapPoints={["50%", "90%"]}
+            onDismiss={() => {
+              setRef.current.dismiss();
+            }}
+          ></BottomSheetModal>
+        </BottomSheetModalProvider>
       </SafeAreaView>
     </View>
   );
