2d7309022dc57857810bb1b4a852e532d3bfcb25
Joe McLean <uncommonjoe@gmail.com>
2022-06-15 16:13:42 -0600

    Interpreted toolbox description to styled components

--- src/pages/toolbox/ToolboxModal.js
+++ src/pages/toolbox/ToolboxModal.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import {
+	View,
+	Image,
+	StyleSheet,
+	SafeAreaView,
+	ScrollView,
+} from 'react-native';
 import page from '../../assets/styles/page.style';
 import { TitleText } from '../../assets/styles/Text';
+import getRNDraftJSBlocks from 'react-native-draftjs-render';
 
 const ToolboxModal = (item) => {
 	const [payload, setPayload] = useState(item.route.params.item);
+	const [convertedText, setConvertedText] = useState();
 
 	const backgroundStyle = (color) => {
-		// Need to convert color with convertColor hook
+		// convert color value to Hex value
 		switch (color) {
 			case 'red':
 				return { backgroundColor: '#802119' };
@@ -23,8 +30,49 @@ const ToolboxModal = (item) => {
 		}
 	};
 
+	// Convert image to something React Native can handle
+	const atomicHandler = (item) => {
+		switch (item.data.type) {
+			case 'image':
+				return (
+					<View key={item.key} style={{ flex: 1 }}>
+						<Image
+							style={{ width: 288, height: 161 }}
+							source={{ uri: item.data.url }}
+						/>
+					</View>
+				);
+			default:
+				return null;
+		}
+	};
+
 	useEffect(() => {
 		setPayload(item.route.params.item);
+
+		if (payload) {
+			// parse JSON object from description string
+			const desc = JSON.parse(payload.description);
+
+			// define settings for getRNDraftJSBlocks
+			const params = {
+				contentState: desc,
+				customStyles,
+				atomicHandler,
+				depthMargin: 32,
+				textProps: {
+					selectable: true,
+				},
+			};
+
+			// interperets JSON object to React Native code
+			// https://github.com/globocom/react-native-draftjs-render
+			const blocks = getRNDraftJSBlocks(params);
+
+			setConvertedText(blocks);
+		} else {
+			setConvertedText('No text found');
+		}
 	}, [item]);
 
 	return (
@@ -37,7 +85,7 @@ const ToolboxModal = (item) => {
 					{payload.title}
 				</TitleText>
 
-				<Text style={styles.white}>{payload.description}</Text>
+				<View>{convertedText}</View>
 			</SafeAreaView>
 		</ScrollView>
 	);
@@ -51,6 +99,80 @@ const styles = StyleSheet.create({
 		color: 'white',
 		fontSize: 20,
 		lineHeight: 30,
+	},
+});
+
+const customStyles = StyleSheet.flatten({
+	unstyled: {
+		fontSize: 18,
+		fontWeight: 'normal',
+		letterSpacing: -0.75,
+		lineHeight: 32,
+		marginBottom: 21,
+		color: 'white',
+	},
+	link: {
+		color: 'white',
+		textDecorationLine: 'underline',
+	},
+	unorderedListItemContainer: {
+		marginBottom: 16,
+		position: 'relative',
+		color: 'white',
+	},
+	unorderedListItemBullet: {
+		marginRight: 10,
+		position: 'relative',
+		top: 14,
+		width: 6,
+		height: 6,
+		alignSelf: 'flex-start',
+		color: 'white',
+	},
+	'unordered-list-item': {
+		fontSize: 18,
+		lineHeight: 32,
+		alignSelf: 'flex-start',
+		flex: 1,
+		color: 'white',
+	},
+	orderedListContainer: {
+		marginBottom: 16,
+		color: 'white',
+	},
+	orderedListItemContainer: {
+		marginBottom: 5,
+		position: 'relative',
+		color: 'white',
+		marginLeft: 0,
+	},
+	orderedListItemNumber: {
+		fontSize: 18,
+		lineHeight: 32,
+		//marginRight: 10,
+		marginHorizontal: 10,
+		alignSelf: 'flex-start',
+		color: 'white',
+	},
+	'ordered-list-item': {
+		alignSelf: 'flex-start',
+		fontSize: 18,
+		lineHeight: 32,
+		flex: 1,
+		color: 'white',
+	},
+	blockquote: {
+		fontWeight: 'bold',
+		lineHeight: 33,
+		paddingTop: 24,
+		marginBottom: 24,
+		fontSize: 33,
+		letterSpacing: -2,
+		color: 'white',
+	},
+	viewAfterList: {
+		marginBottom: 32,
+		color: 'white',
 	},
 });
 
