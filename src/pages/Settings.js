227853d09f9452e111e82accbe7674a2105feda6
Joe McLean <uncommonjoe@gmail.com>
2022-06-17 14:08:55 -0600

    Changed sheets background to white and bottom bar icon colors

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,58 +1,59 @@
-import React from "react";
-import { View, SafeAreaView, TouchableOpacity, Text } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { TitleText } from "../assets/styles/Text";
-import button from "../assets/styles/button.style";
-import page from "../assets/styles/page.style";
-import useAuthentication from "../utils/hooks/useAuthentication";
-import { signOut, getAuth } from "firebase/auth";
-import NavService from "../navigation/NavService";
+import React from 'react';
+import { View, SafeAreaView, TouchableOpacity, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { TitleText } from '../assets/styles/Text';
+import button from '../assets/styles/button.style';
+import page from '../assets/styles/page.style';
+import useAuthentication from '../utils/hooks/useAuthentication';
+import { signOut, getAuth } from 'firebase/auth';
+import NavService from '../navigation/NavService';
 
 export default function Settings(props) {
-  const { user } = useAuthentication();
-  const auth = getAuth();
+	const { user } = useAuthentication();
+	const auth = getAuth();
 
-  return (
-    <View
-      style={[
-        page.container,
-        {
-          height: 100,
-          width: "100%",
-          backgroundColor: "#454C57",
-        },
-      ]}
-    >
-      <SafeAreaView>
-        <StatusBar style="dark" />
+	return (
+		<View
+			style={[
+				page.container,
+				{
+					height: 100,
+					width: '100%',
+					backgroundColor: 'white',
+				},
+			]}
+		>
+			<SafeAreaView>
+				<StatusBar style='dark' />
 
-        <Text
-          style={{
-            color: "#fff",
-            fontSize: 20,
-            fontWeight: "500",
-            textTransform: "uppercase",
-            marginBottom: 25,
-          }}
-        >
-          Hello {user?.displayName}
-        </Text>
+				<Text
+					style={{
+						fontSize: 20,
+						fontWeight: '500',
+						textTransform: 'uppercase',
+						marginBottom: 25,
+					}}
+				>
+					Hello {user?.displayName}
+				</Text>
 
-        <View style={page.section}>
-          <TouchableOpacity
-            style={[button.button, button.green]}
-            onPress={() => {
-              signOut(auth);
-              global.usrSettngs = null;
-              global.user = null;
-              global.userReadings = [];
-              NavService.resetStack("AuthStack");
-            }}
-          >
-            <Text style={[button.text, { color: "white" }]}>Sign Out</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </View>
-  );
+				<View style={page.section}>
+					<TouchableOpacity
+						style={[button.button, button.green]}
+						onPress={() => {
+							signOut(auth);
+							global.usrSettngs = null;
+							global.user = null;
+							global.userReadings = [];
+							NavService.resetStack('AuthStack');
+						}}
+					>
+						<Text style={[button.text, { color: 'white' }]}>
+							Sign Out
+						</Text>
+					</TouchableOpacity>
+				</View>
+			</SafeAreaView>
+		</View>
+	);
 }
