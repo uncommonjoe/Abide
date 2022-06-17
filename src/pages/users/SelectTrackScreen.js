9928df6111c81d171d1046c0a8fab21e6bd543b3
Joe McLean <uncommonjoe@gmail.com>
2022-06-17 15:14:23 -0600

    Updated select track UI

--- src/pages/users/SelectTrackScreen.js
+++ src/pages/users/SelectTrackScreen.js
@@ -1,132 +1,137 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-  TouchableOpacity,
-  ActivityIndicator,
-  Dimensions,
-} from "react-native";
-import { StatusBar } from "expo-status-bar";
-let { height, width } = Dimensions.get("window");
-import { TitleText } from "../../assets/styles/Text";
-import button from "../../assets/styles/button.style";
-import input from "../../assets/styles/input.style";
-import page from "../../assets/styles/page.style";
-import useAuthentication from "../../utils/hooks/useAuthentication";
-import { addTrack } from "../../config/firebase";
-import { useNavigation } from "@react-navigation/native";
+	StyleSheet,
+	View,
+	SafeAreaView,
+	ScrollView,
+	TouchableOpacity,
+	ActivityIndicator,
+	Dimensions,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+let { height, width } = Dimensions.get('window');
+import { TitleText, Text } from '../../assets/styles/Text';
+import button from '../../assets/styles/button.style';
+import input from '../../assets/styles/input.style';
+import page from '../../assets/styles/page.style';
+import useAuthentication from '../../utils/hooks/useAuthentication';
+import { addTrack } from '../../config/firebase';
+import { useNavigation } from '@react-navigation/native';
 
 const SelectTrackScreen = (props) => {
-  const [option, setOption] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const user = useAuthentication();
-  const navigation = useNavigation();
+	const [option, setOption] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const user = useAuthentication();
+	const navigation = useNavigation();
 
-  const data = [
-    { value: "Track 1" },
-    { value: "Track 2" },
-    { value: "Track 3" },
-  ];
+	const data = [
+		{ value: 'Track 1' },
+		{ value: 'Track 2' },
+		{ value: 'Track 3' },
+	];
 
-  const submit = async (track) => {
-    global.usrSettngs = { ...user.user, track };
-    if (user && track) {
-      let data = {
-        user: user.user,
-        option: track,
-      };
+	const submit = async (track) => {
+		global.usrSettngs = { ...user.user, track };
+		if (user && track) {
+			let data = {
+				user: user.user,
+				option: track,
+			};
 
-      setLoading(true);
-      await addTrack(data);
-      setLoading(false);
-      navigation.navigate("UserStack", { user: user.user });
-    } else {
-      console.log("no user");
-    }
-  };
+			setLoading(true);
+			await addTrack(data);
+			setLoading(false);
+			navigation.navigate('UserStack', { user: user.user });
+		} else {
+			console.log('no user');
+		}
+	};
 
-  return (
-    <>
-      <ScrollView
-        style={[page.container, { paddingBottom: 50 }]}
-        contentInsetAdjustmentBehavior="automatic"
-      >
-        <SafeAreaView>
-          <StatusBar style="dark" />
+	return (
+		<>
+			<ScrollView
+				style={[page.container, { paddingBottom: 50 }]}
+				contentInsetAdjustmentBehavior='automatic'
+			>
+				<SafeAreaView>
+					<StatusBar style='dark' />
 
-          <TitleText style={{ marginTop: 40 }}>Select Track to Begin</TitleText>
+					<TitleText style={{ marginTop: 40 }}>
+						Select Track to Begin
+					</TitleText>
 
-          <View style={page.section}>
-            <Text style={input.title}>Track 1</Text>
+					<View style={page.section}>
+						<TouchableOpacity
+							style={[button.button, button.blue]}
+							onPress={() => submit('Track 1')}
+						>
+							<Text style={button.text}>Track 1</Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity
-              style={[button.button, button.blue]}
-              onPress={() => submit("Track 1")}
-            >
-              <Text style={button.text}>New Testament</Text>
-              <Text style={[button.text, local.textXXL]}>3X</Text>
-            </TouchableOpacity>
-          </View>
+						<Text style={local.description}>
+							Read the New Testament three times by reading two
+							chapters a day for five days&nbsp;a&nbsp;week.
+						</Text>
+					</View>
 
-          <View style={page.section}>
-            <Text style={input.title}>Track 2</Text>
+					<View style={page.section}>
+						<TouchableOpacity
+							style={[button.button, button.tan]}
+							onPress={() => submit('Track 2')}
+						>
+							<Text style={button.text}>Track 2</Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity
-              style={[button.button, button.tan]}
-              onPress={() => submit("Track 2")}
-            >
-              <Text style={button.text}>Track 1</Text>
-              <Text style={button.text}>+</Text>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={[button.text, local.textXXL]}>2X</Text>
-                <View style={{ textAlign: "left", paddingLeft: 20 }}>
-                  <Text style={button.text}>Proverbs</Text>
-                  <Text style={button.text}>Through</Text>
-                  <Text style={button.text}>Psalms</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </View>
+						<Text style={local.description}>
+							Read the New Testament three times and
+							Psalms/Proverbs twice by reading three chapters a
+							day for five days&nbsp;a&nbsp;week.
+						</Text>
+					</View>
 
-          <View style={page.section}>
-            <Text style={input.title}>Track 3</Text>
-            <TouchableOpacity
-              style={[button.button, button.red]}
-              onPress={() => submit("Track 3")}
-            >
-              <Text style={button.text}>Track 2</Text>
-              <Text style={button.text}>+</Text>
-              <Text style={button.text}>Old Testament</Text>
-              <Text style={[button.text, local.textXXL]}>1X</Text>
-            </TouchableOpacity>
-          </View>
-        </SafeAreaView>
-      </ScrollView>
-      {loading ? (
-        <View
-          style={{
-            height,
-            width,
-            position: "absolute",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "rgba(0,0,0,0.7)",
-          }}
-        >
-          <ActivityIndicator size="large" color="#fff" />
-        </View>
-      ) : null}
-    </>
-  );
+					<View style={page.section}>
+						<TouchableOpacity
+							style={[button.button, button.red]}
+							onPress={() => submit('Track 3')}
+						>
+							<Text style={button.text}>Track 3</Text>
+						</TouchableOpacity>
+
+						<Text style={local.description}>
+							Read the New Testament three times, Psalms/Proverbs
+							twice and the Old Testament once by reading five
+							chapters a day for five days&nbsp;a&nbsp;week.
+						</Text>
+					</View>
+				</SafeAreaView>
+			</ScrollView>
+			{loading ? (
+				<View
+					style={{
+						height,
+						width,
+						position: 'absolute',
+						justifyContent: 'center',
+						alignItems: 'center',
+						backgroundColor: 'rgba(0,0,0,0.7)',
+					}}
+				>
+					<ActivityIndicator size='large' color='#fff' />
+				</View>
+			) : null}
+		</>
+	);
 };
 
 const local = StyleSheet.create({
-  textXXL: {
-    fontSize: 54,
-  },
+	description: {
+		fontSize: 14,
+		textAlign: 'center',
+		marginTop: 10,
+		marginBottom: 30,
+		paddingHorizontal: 10,
+		color: '#454C57',
+	},
 });
 
 export default SelectTrackScreen;
