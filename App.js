abf830612187398680253b6095c4e8320a1891ea
Joe McLean <uncommonjoe@gmail.com>
2022-06-18 15:39:53 -0600

    removed unused code

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import "./src/config/firebase";
-import RootNavigation from "./src/navigation";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import NavService from "./src/navigation/NavService";
-import { Store } from "./src/redux";
-import { Provider } from "react-redux";
-import { View } from "react-native";
+import React from 'react';
+import './src/config/firebase';
+import RootNavigation from './src/navigation';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import NavService from './src/navigation/NavService';
+import { Store } from './src/redux';
+import { Provider } from 'react-redux';
 
 export default function App() {
-  return (
-    <Provider store={Store}>
-      <NavigationContainer ref={(ref) => NavService.setTopLevelNavigator(ref)}>
-        <RootNavigation />
-      </NavigationContainer>
-    </Provider>
-  );
+	return (
+		<Provider store={Store}>
+			<NavigationContainer
+				ref={(ref) => NavService.setTopLevelNavigator(ref)}
+			>
+				<RootNavigation />
+			</NavigationContainer>
+		</Provider>
+	);
 }
