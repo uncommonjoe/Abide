9a52e772b1f85a803cc7b40c78f9527a003b616a
Joe McLean <uncommonjoe@gmail.com>
2022-06-23 15:45:28 -0600

    changes to make build work

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,29 @@
-module.exports = {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    resolver: {
-      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
-    },
-  };
+// module.exports = {
+//     transformer: {
+//       getTransformOptions: async () => ({
+//         transform: {
+//           experimentalImportSupport: false,
+//           inlineRequires: true,
+//         },
+//       }),
+//     },
+//     resolver: {
+//       sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
+//     },
+//   };
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = (async () => {
+	const config = await getDefaultConfig(__dirname);
+
+	const { transformer, resolver } = config;
+
+	config.resolver = {
+		...resolver,
+		assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+		sourceExts: [...resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx', 'cjs'],
+	};
+
+	return config;
+})();
